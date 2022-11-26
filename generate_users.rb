5668795ab2aeb2c38d8e4208c90870ae035cc4d8
ti-aiuto <ti.aiuto@outlook.jp>
2022-11-27 00:27:23 +0900

    :pencil2: ユーザ生成

---
+++ generate_users.rb
@@ -0,0 +1,26 @@
+require 'active_support/all'
+require 'csv'
+
+users = []
+
+
+base_datetime = Time.new(2022, 11, 10)
+100.times do |index|
+    registered_at = base_datetime + rand(20).days + rand(24).hours + rand(3600)
+    confirmed_at = nil
+    if rand() <= 0.8
+        if rand() <= 0.8
+            confirmed_at = registered_at + rand(3600 * 3)
+        else
+            confirmed_at = registered_at + rand(8).days + rand(3600 * 3)
+        end
+    end
+    users << [200001 + index, registered_at.utc.iso8601, confirmed_at&.utc&.iso8601]
+end
+
+CSV.open('users.csv','w') do |csv|
+    csv << ['id', 'registered_at', 'confirmed_at']
+    users.each do |user|
+        csv << user
+    end
+end
